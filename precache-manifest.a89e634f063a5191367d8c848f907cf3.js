aff9ee6575f51ce40846f9ac9955246706f24110
sanghaarsha <sanghaarsha@gmail.com>
2021-07-14 09:11:13 +0545

    Updates

---
+++ precache-manifest.a89e634f063a5191367d8c848f907cf3.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bfb4635707381efe6b9cdf2d30116af3",
+    "url": "/keeper-app-react/index.html"
+  },
+  {
+    "revision": "7fd484ac467f479c58b8",
+    "url": "/keeper-app-react/static/js/2.c2b5d5d6.chunk.js"
+  },
+  {
+    "revision": "6ef44c617db1437a392e",
+    "url": "/keeper-app-react/static/js/main.1f27d88d.chunk.js"
+  },
+  {
+    "revision": "70a42cb2e25b4b770fd5",
+    "url": "/keeper-app-react/static/js/runtime-main.17487c9c.js"
+  }
+]);
